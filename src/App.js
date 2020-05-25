3bcc58d8e4dc9a14455c1e55aa12cf6649a5b5d9
himanshuraj9062 <himanshuraj9062@gmail.com>
2020-05-25 16:58:02 +0530

    Added Lazy Loadning
    Lazy loading + emotion  component styles css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState ,lazy,Suspense} from 'react';
 import './App.css';
 import { Router } from "@reach/router";
 // import Pet from './Pet';
-import SearchParams from './serachParam';
-import Details from "./Details";
+// import SearchParams from './serachParam';
+import NavBar from './NavBar';
+
+// import Details from "./Details";
 import ThemeContext from './ThemeContext';
+const Details = lazy( () => import('./Details') )  
+const SearchParams = lazy( () => import('./serachParam') )  
 
 
 function App() {    
@@ -13,11 +17,16 @@ function App() {
     <ThemeContext.Provider value={theme}>
 
         <div>
-          <h1 id = "something">"Adopt Me!"</h1>
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+
+          {/* <h1 id = "something">"Adopt Me!"</h1> */}
+
+          <NavBar></NavBar>
+          <Suspense fallback = {<h1>Loading Route</h1>}>
+              <Router>
+                <SearchParams path="/" />
+                <Details path="/details/:id" />
+              </Router>
+          </Suspense>
         </div>
         </ThemeContext.Provider>
 
--- src/Details.js
+++ src/Details.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React,{lazy}from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ThemeContext from "./ThemeContext";
 import ErrorBoundary from "./ErrorBoundary";
 import { navigate } from "@reach/router";
-import Modal from "./Modal";
+// import Modal from "./Modal";
+
+
+const Modal = lazy( () => import('./Modal') )  
+
 class Details extends React.Component {
   state = { loading: true, showModal: false };
   componentDidMount() {
